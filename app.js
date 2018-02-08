d1c436731fc66191a8c58bd151283b69c089798e
Filipe <filipe.echevengua@softerize.com>
2018-02-07 22:26:32 -0200

    mudança datas teste backend

--- app.js
+++ app.js
@@ -36,12 +36,12 @@ app.get('/weatherData', function(req, res, next) {
     let records = [];
     collection.find({
         "ts": {
-            $gte: "2017-09-15 00:00:00.000",
-            $lte: "2017-09-15 23:59:59.000"
+            $gte: "2017-09-18 00:00:00.000",
+            $lte: "2017-09-18 23:59:59.000"
             //$gte: new Date(req.body.date + "T00:00:00.000Z"),
             //$lte: new Date(req.body.date + "T23:59:59.000Z")
         },
-        "device_code": 888011
+        "device_code": 888039
     }).toArray(function(err, results) {
         responseData.payload = results;
         res.contentType('application/json');
@@ -51,9 +51,17 @@ app.get('/weatherData', function(req, res, next) {
 
 app.get('/heatmap', function(req, res, next) {
     let records = [];
+    let fullDate;
+
+    if(req.query.date){
+        fullDate = req.query.date ? req.query.date : '';
+    }
+
+    console.log(fullDate);
+
     collection.find({
         "device_code": 888039,
-	    "sensor_code": 2
+        "sensor_code": 2
     }).toArray(function(err, results) {
         let oldDate = '';
         let mediaTemperatura = [];
@@ -70,12 +78,14 @@ app.get('/heatmap', function(req, res, next) {
                 let semana = dateFormat(new Date(valor.ts), "W");
                 let diaDaSemana = dateFormat(new Date(valor.ts), "N");
                 let diaDoMes = dateFormat(new Date(valor.ts), "dd");
+                let dataCompleta = dateFormat(new Date(valor.ts), "yyyy-mm-dd HH:MM:ss");
 
                 mediaTemperatura.push({
                     day: diaDaSemana,
                     week: semana,
                     value: valorTemperatura/count,
-                    date: diaDoMes
+                    dateDay: diaDoMes,
+                    fullDate: dataCompleta
                 });
                 oldDate = newDate;
                 count = 1;
@@ -96,14 +106,15 @@ app.post('/dateWeatherData', function(req, res, next) {
     let records = [];
     collection.find({
         "ts": {
-            $gte: new Date("2017-05-05T00:00:00.000Z"),
-            $lte: new Date("2017-05-05T23:59:59.000Z")
+            $gte: "2017-09-18 00:00:00.000",
+            $lte: "2017-09-18 23:59:59.000"
             //$gte: new Date(req.body.date + "T00:00:00.000Z"),
             //$lte: new Date(req.body.date + "T23:59:59.000Z")
         },
         "sensor_code": parseInt(req.body.sensor_code),
-        "device_code": 888011
+        "device_code": 888039
     }).toArray(function(err, results) {
+        console.log(results);
         responseData.payload = results;
         res.contentType('application/json');
         res.send(JSON.stringify(responseData));
